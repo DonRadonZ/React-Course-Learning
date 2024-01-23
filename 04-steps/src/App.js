0b379aad22c94463ae0f96464b5739e2b0361d84
Supachai Sinkaseam <supachai27102000@gmail.com>
2024-01-24 00:51:12 +0700

    update setState

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -15,11 +15,14 @@ export default function App() {
 
 
  function handlePrevious() {
-  if(step > 1) setStep(step - 1);
+  if(step > 1) setStep((s) => s - 1);
  }
 
  function handleNext() {
-  if(step < 3) setStep(step + 1 );
+   if (step < 3) {
+     setStep((s) => s + 1);
+     // setStep((s) => s + 1);
+   };
 
   //BAD PRACTICE
   // test.name = "NY"
@@ -28,7 +31,7 @@ export default function App() {
 
   return( 
   <>
-  <button className="close" onClick={() => setIsOpen(!isOpen)}>&times;</button>
+  <button className="close" onClick={() => setIsOpen((is) => !is)}>&times;</button>
     { isOpen && (
       <div className="steps" style={{backgroundColor: "#DCF2F1"}}>
         <div className="numbers">
