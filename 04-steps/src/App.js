28f1167168010b6211e87c6cea58230e238dd75f
Supachai Sinkaseam <supachai27102000@gmail.com>
2024-02-07 00:14:50 +0700

    another props

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -9,6 +9,15 @@ export default function App() {
   return (
     <div>
       <Steps />
+      <StepMessage step={1}>
+        <p>Pass in content</p>
+        <p>✌️</p>
+      </StepMessage>
+      <StepMessage step={2}>
+        <p>Read children prop</p>
+        <p>🖐️</p>
+      </StepMessage>
+      
     </div>
   )
 }
@@ -48,11 +57,17 @@ function Steps() {
           <div className={step >= 3 ? "active" : ""}>3</div>
         </div>
 
-        <p className="message">
-        Step {step}: {messages[step - 1]}
-        {/* {test.name} */}
-        </p>
-
+        
+          
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+          <div className="buttons">
+          <Button
+            bgColor="#e7e7e7"
+            textColor="#333"
+            onClick={() => alert(`Learn how to ${messages[step - 1]}`)}          
+          >Learn how
+            </Button>
+            </div>
         <div className="buttons">
             <Button
               bgColor="#7FC7D9"
@@ -70,6 +85,16 @@ function Steps() {
   </>
     
 )
+}
+
+function StepMessage({ step, children }) {
+  
+  return(
+  <div className="message">
+         <h3>Step {step}</h3>
+        {children}
+    </div>
+  )
 }
 
 function Button({textColor, bgColor, onClick, children}) {
