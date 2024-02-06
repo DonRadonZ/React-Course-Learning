534412a0076aac46ea2d5f4d76d1f32abd0b72fd
RadonZ <supachai27102000@gmail.com>
2024-02-06 17:27:44 +0700

    make clearing sorting and calculating

--- 05-want-buy/src/App.js
+++ 05-want-buy/src/App.js
@@ -1,24 +1,52 @@
 import {useState} from 'react'
-const initialItems = [
-  { id: 1, description: "Milk", quantity: 2, bought: false },
-  { id: 2, description: "Candy", quantity: 12, bought: false },
-  { id: 2, description: "Softener", quantity: 6, bought: true },
-];
+
+// const initialItems = [
+//   { id: 1, description: "Milk", quantity: 2, bought: false },
+//   { id: 2, description: "Candy", quantity: 12, bought: false },
+//   { id: 3, description: "Softener", quantity: 6, bought: false },
+// ];
 
 
 export default function App() {
   const [items, setItems] = useState([]);
+  
 
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items)=> items.filter((item)=>item.id !== 
+    id));
+  }
+
+  function handleToggleItem(id){
+    setItems((items) => 
+    items.map((item) => 
+      item.id === id ? {...item, bought: !item.bought}
+      : item
+      )
+    );
+  };
+
+  function handleClearItem(){
+    const confirmed = window.confirm('Are you sure you want to delete all items?');
+    
+    if(confirmed)
+    setItems([]);
   }
 
   return(
     <div className="app">
       <Logo/>
       <Form onAddItems={handleAddItems} />
-      <BuyingList items={items} />
-      <Stats/>
+      <BuyingList 
+      items={items} 
+      onDeleteItem={handleDeleteItem}
+      onToggleItem={handleToggleItem}
+      onClearItem={handleClearItem}
+       />
+      <Stats items={items}/>
     </div>
   )
 }
@@ -78,32 +106,73 @@ function Form({onAddItems}) {
   );
 }
 
-function BuyingList({items}) {
-  
+function BuyingList({items, onDeleteItem, onToggleItem, onClearItem}) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if(sortBy === 'input') sortedItems = items;
+
+  if(sortBy === 'description') sortedItems = items
+  .slice()
+  .sort((a,b) => a.description.localeCompare(
+    b.description));
+
+  if(sortBy === 'bought') 
+  sortedItems = items
+.slice()
+.sort((a,b) => Number(a.bought) - Number(b.bought));
+
   return (
   <div className="list">
       <ul>
-        {items.map((item) => (
-          <Item item={item} key={item.id} />
+        {sortedItems.map((item) => (
+          <Item item={item} 
+          onDeleteItem={onDeleteItem} 
+          onToggleItem={onToggleItem}
+          key={item.id} />
         ))}
       </ul>
+
+      <div className='actions'> 
+        <select value={sortBy} onChange={e=>setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="bought">Sort by bought status</option>
+        </select>
+        <button onClick={onClearItem}>Clear list</button>
+      </div>
   </div>
   )
 } 
 
-function Item({item}){
+function Item({item, onDeleteItem, onToggleItem}){
+  
   return(
     <li>
-      <span style={item.packed ? {textDecoration: 'line-through'} : {}}>
+      <input type="checkbox" value={item.bought} 
+      onChange={()=> onToggleItem(item.id)} />
+      <span style={item.bought ? {textDecoration: 'line-through'} : {}}>
         {item.quantity} {item.description} 
       </span>
-      <button>❌</button>    
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>    
     </li>
   )
 }
 
-function Stats() {
-  return <footer className="stats">
-   <em> You have X items on your list, and you already packed X (X%) </em>
+function Stats({ items }) {
+  if(!items.length) return(
+  <p className='stats'><em>Start adding some items to your buying List</em></p>)
+
+  const numItems = items.length;
+  const numBought = items.filter((item) => item.bought).length;
+  const percentage = Math.round((numBought / numItems) * 100)
+  return (<footer className="stats">
+   <em>
+    {percentage === 100 ? 'You got everything!'  
+    : `You have ${numItems} items on your list, 
+    and you already packed ${numBought} (${percentage}%)`} 
+    </em>
   </footer>
+  );
 } 
