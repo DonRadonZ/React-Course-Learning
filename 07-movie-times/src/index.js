2e685cf0a58be53b2e63a799cad64d8f64724ea4
RadonZ <supachai27102000@gmail.com>
2024-02-13 11:45:01 +0700

    finish star

--- 07-movie-times/src/index.js
+++ 07-movie-times/src/index.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 import StarRating from './components/StarRating/starrating';
 
 
+function Test(){
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+  <div>
+    <StarRating color="blue" onSetRating={setMovieRating}/>
+    <p>This movie was rated {movieRating} stars</p>
+    </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-
-    <StarRating maxRating={10}/>
+    <StarRating maxRating={10} messages={["Terrible","Bad","Okay", "Good", "Amazing"]}/>
+    <StarRating size={24} color="red" className="test" defaultRating={2}/>
+    <Test/>
   </React.StrictMode>
 );
 
