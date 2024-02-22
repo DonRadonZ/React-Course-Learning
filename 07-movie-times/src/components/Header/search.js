663b8a97fb37bf6b7735c1ade56bf509e80b33fc
RadonZ <supachai27102000@gmail.com>
2024-02-22 23:40:31 +0700

    add useKey

--- 07-movie-times/src/components/Header/search.js
+++ 07-movie-times/src/components/Header/search.js
@@ -1,39 +1,23 @@
-import { useEffect, useRef } from "react"
+import { useRef } from "react"
+import { useKey } from "../useKey/useKey"
 
-export default function Search({query,setQuery}){
+export default function Search({ query, setQuery }) {
   const inputEl = useRef(null);
 
-  useEffect(
-    function () {
-      function callback(e) {
-        if (document.activeElement === inputEl.current) return;
+  useKey("Enter", function () {
+    if (document.activeElement === inputEl.current) return;
+    inputEl.current.focus();
+    setQuery("");
+  });
 
-        if (e.code === "Enter") {
-          inputEl.current.focus();
-          setQuery("");
-        }
-      }
-
-      document.addEventListener("keydown", callback);
-      return () => document.addEventListener("keydown", callback);
-    },
-    [setQuery]
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
+    />
   );
-
-    // useEffect(function(){
-    //   const el = document.querySelector('.search');
-    //   console.log(el);
-    //   el.focus();
-    // },[]);  
-
-    return(
-        <input
-          className="search"
-          type="text"
-          placeholder="Search movies..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          ref={inputEl}
-        />
-    )
 }
