1d24eb173c26aff24790efb4f90e561501f8fedf
RadonZ <supachai27102000@gmail.com>
2024-02-28 18:12:48 +0700

    add resource

---
+++ 10-react-quiz/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import DateCounter from './utils/dateCount/DateCounter';
+
+
+function App() {
+  return (
+    <div>
+      
+      <DateCounter/>
+    </div>
+  );
+}
+
+export default App;
