7caca155342632c553dd6d02a7f8106b3f650c66
Supachai Sinkaseam <supachai27102000@gmail.com>
2024-03-03 07:55:15 +0700

    add infobox

--- additional-project/react-typescript-practice/course-goal/src/components/CourseGoalList.tsx
+++ additional-project/react-typescript-practice/course-goal/src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
 import CourseGoal from "./CourseGoal";
 import { type CourseGoal as CourseGoals } from "../App";
+import InfoBox from "./InfoBox";
+import { ReactNode } from "react";
 
 type CourseGoalListProps = {
     goals: CourseGoals[];
@@ -7,8 +9,27 @@ type CourseGoalListProps = {
 }
 
 export default function CourseGoalList({goals, onDeleteGoal}: CourseGoalListProps) {
-    
+  if (goals.length === 0) {
     return (
+      <InfoBox mode="hint" severity="low">
+        You have no course goals yet. Start adding some!
+      </InfoBox>
+    );
+  }  
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 5) { 
+    warningBox = (
+      <InfoBox mode="warning" severity="high">
+        You're collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
+
+  return (
+    <>
+        {warningBox}
         <ul>
         {goals.map((goal) => (
           <li key={goal.id}>
@@ -18,5 +39,6 @@ export default function CourseGoalList({goals, onDeleteGoal}: CourseGoalListProp
           </li>
         ))}
       </ul>
+      </>
     )
 }
