0a343880f19c068445a38f5baa2242591624225f
RadonZ <supachai27102000@gmail.com>
2024-03-04 11:57:16 +0700

    finish timer

--- additional-project/react-typescript-practice/react-timer/src/components/Header.tsx
+++ additional-project/react-typescript-practice/react-timer/src/components/Header.tsx
@@ -1,11 +1,21 @@
 import Button from './UI/Button.tsx';
 
+import {  useTimerContext } from '../store/timers-context.tsx';
+
 export default function Header() {
+  const timersCtx = useTimerContext();
+
+  
   return (
     <header>
       <h1>ReactTimer</h1>
 
-      <Button>Stop Timers</Button>
+      <Button 
+      onClick={
+        timersCtx.isRunning? timersCtx.stopTimers : timersCtx.startTimers}
+        >
+            {timersCtx.isRunning ? 'Stop': 'Start'}
+      </Button>
     </header>
   );
 }
