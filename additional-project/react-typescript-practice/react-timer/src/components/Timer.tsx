936d8613aca7386835fdcfa036e4f450922e9b36
RadonZ <supachai27102000@gmail.com>
2024-03-05 14:16:58 +0700

    add resource

--- additional-project/react-typescript-practice/react-timer/src/components/Timer.tsx
+++ additional-project/react-typescript-practice/react-timer/src/components/Timer.tsx
@@ -1,13 +1,47 @@
+import { useEffect, useRef, useState } from 'react';
+
 import Container from './UI/Container.tsx';
-import { Timer as TimerProps } from '../store/timers-context.tsx';
-
-
+import { Timer as TimerProps, useTimerContext } from '../store/timers-context.tsx';
 
 export default function Timer({name,duration}: TimerProps) {
+  const interval = useRef<number | null>(null);
+  const [remainingTime, setRemainingTime] =useState(duration * 1000);
+  const {isRunning} = useTimerContext();
+  
+  
+  if(remainingTime <= 0 && interval.current){
+    clearInterval(interval.current);
+  }
+
+  useEffect(() => {
+    let timer: number;
+
+    if(isRunning){
+    timer = setInterval(function() {
+      setRemainingTime((prevTime) => {
+        if (prevTime <= 0) {
+          return prevTime;
+        }
+        return prevTime - 50;
+      });
+    }, 50);
+    interval.current = timer;
+
+  } else if (!isRunning && interval.current) {
+     clearInterval(interval.current)
+  }
+
+    return () => clearInterval(timer);
+  }, [isRunning]);
+
+  const formatterRemainingTime = (remainingTime / 1000).toFixed(2);
+  
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p><progress max={duration * 1000} value={remainingTime} /></p>
+      <p>{formatterRemainingTime}</p>
     </Container>
   );
 }
