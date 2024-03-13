536e959fc503753071d61312c9192996a3fa10ef
RadonZ <supachai27102000@gmail.com>
2024-03-13 16:32:01 +0700

    add reducer

---
+++ 10-react-quiz/src/utils/dateCount/dateCounterReducer.ts
@@ -0,0 +1,36 @@
+export interface DateCountState {
+    count: number;
+    step: number;
+    
+}
+
+export type CountAction = 
+    | {type: 'inc' | 'dec'}
+    | {type: "SET_COUNT"; newCount: number}
+    | {type: "SET_STEP"; newStep: number}
+    | {type: "RESET"}
+    
+  
+
+export function dateCountReducer(
+    state: DateCountState,
+    action: CountAction
+): DateCountState {
+    console.log("state", state)
+    console.log("action", action)
+
+    switch (action.type){
+        case "inc":
+            return {...state, count: state.count + state.step}
+        case "dec":
+            return {...state, count: state.count - state.step}
+        case "SET_COUNT":
+            return {...state, count: action.newCount}
+        case "SET_STEP":
+            return {...state, step : action.newStep}
+        case "RESET":
+            return {...state,count: 0, step: 1}
+        default:
+            return state;
+    }
+}
