7a83b64364d61fed500ef52fc5968edadff85ebb
RadonZ <supachai27102000@gmail.com>
2024-03-20 13:23:03 +0700

    update question

--- 10-react-quiz/src/components/question/Question.tsx
+++ 10-react-quiz/src/components/question/Question.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Options from '../option/Option';
 export default function Question({ question, dispatch, answer }) {
-    console.log(question);
   return (
       <div>
           <h4>{question.question}</h4>
