5edb7ba11b866b12401c5a54491db45152ca8828
RadonZ <supachai27102000@gmail.com>
2024-03-25 18:25:23 +0700

    update resource

--- 11-worldwise/src/App.tsx
+++ 11-worldwise/src/App.tsx
@@ -1,9 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Product from './page/Product/Product';
+import Pricing from './page/Pricing/Pricing';
+import Login from './page/Login/Login';
+import Homepage from './page/Home/Homepage';
+import PageNotFound from './page/PageNotFound/PageNotFound';
+import AppLayout from './page/AppLayout/AppLayout';
 
 
 export default function App() {
   return (
-    <div>
-      WorldWise
-    </div>
+
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="product" element={<Product />} />
+      <Route path="pricing" element={<Pricing />} />
+      <Route path="app" element={<AppLayout />} />
+      <Route path="Login" element={<Login/>} />
+      <Route path="*" element={<PageNotFound/>} />
+    </Routes>
+    </BrowserRouter>
   )
 }
